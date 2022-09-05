505cc74daeaa6bd73fd88826ec381237b4d9fce1
ERKELSOT <1256029807@qq.com>
2022-09-05 21:08:40 +0800

    ✨ feat(custom): release bumpp
    add bumpp to publish

--- packages/wujie-cli/template/examples/main-vite/src/views/Angular12.vue
+++ packages/wujie-cli/template/examples/main-vite/src/views/Angular12.vue
@@ -10,7 +10,7 @@ export default {
     return {
       url: hostMap("//localhost:7400/"),
       // 修正iframe的url，防止github pages csp报错
-      attrs: process.env.NODE_ENV === "production" ? { src: hostMap("//localhost:7400/") } : {},
+      // attrs: process.env.NODE_ENV === "production" ? { src: hostMap("//localhost:7400/") } : {},
     };
   },
 };
